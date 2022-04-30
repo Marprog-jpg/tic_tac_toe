f2596fdb6d6d42ade22cb8cc4e2c3bbacb276e0f
Alle-marchio <78793701+Alle-marchio@users.noreply.github.com>
2022-04-30 12:37:47 +0200

    create activity human vs human

---
+++ app/src/main/java/com/example/tic_tac_toe/humanVsHuman.java
@@ -0,0 +1,16 @@
+package com.example.tic_tac_toe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class humanVsHuman extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.human_vs_human);
+        System.out.println("ciao");
+        System.out.println("ciao sono alle");
+    }
+}

ca96841a46f36083a795b005aeea1b2e74d04713
Marprog-jpg <78869306+Marprog-jpg@users.noreply.github.com>
2022-05-07 17:17:51 +0200

    Text in button changes after the user taps on one.

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.java
@@ -2,14 +2,32 @@ package com.example.tic_tac_toe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    @Override
+    private Button button;
+    private Button button2;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("ciao");
         System.out.println("ciao sono alle");
+
+        button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, humanVsHuman.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/tic_tac_toe/humanVsComputer.java
+++ app/src/main/java/com/example/tic_tac_toe/humanVsComputer.java
@@ -1,16 +1,59 @@
 package com.example.tic_tac_toe;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class humanVsComputer extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class humanVsComputer extends AppCompatActivity implements View.OnClickListener{
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+    private Button button9;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.human_vs_computer);
-        System.out.println("ciao");
-        System.out.println("ciao sono alle");
+        setContentView(R.layout.activity_human_vs_computer);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+
+        button5 = (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(this);
+
+        button6 = (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(this);
+
+        button7 = (Button) findViewById(R.id.button7);
+        button7.setOnClickListener(this);
+
+        button8 = (Button) findViewById(R.id.button8);
+        button8.setOnClickListener(this);
+
+        button9 = (Button) findViewById(R.id.button9);
+        button9.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        button1.setText("X");
     }
 }
